4bfbabfbbd00cf007c5e3c3a319c66128ba49946
Arvind sharma <arvind17151147@gmail.com>
2025-08-07 22:14:11 +0530

    Update and rename README.md to assignment.java
    java assignment

---
+++ assignment.java
@@ -0,0 +1,85 @@
+
+// Q1. Create a class StudentInfo with a method displayInfo() that prints your name, age, and grade.
+
+// public class studentinfo {
+
+//     public void displayinfo() {
+//         System.out.println("Name: Aman Sharma");
+//         System.out.println("Age: 16");
+//         System.out.println("Grade: 10th");
+//     }
+
+//     public static void main(String[] args) {
+//         studentinfo obj = new studentinfo();
+//         obj.displayinfo();
+//     }
+// }
+
+// Q2. Create a class Calculator with a method addNumbers() that adds two numbers and prints the result.
+
+// public class calculator {
+
+//     public void addno() {
+//         int a = 10;
+//         int b = 20;
+//         int sum = a + b;
+//         System.out.println("sum = " + sum);
+//     }
+
+//     public static void main(String[] args) {
+//         calculator obj = new calculator();
+//         obj.addno();
+//     }
+// }
+
+// Q3. Create a class Greeting with a method sayHello() that takes your name as a parameter and prints:
+
+
+// public class greeting {
+
+//     public void say(String name) {
+//         System.out.println("hello, " + name + "! welcome to java programming.");
+//     }
+
+//     public static void main(String[] args) {
+//         greeting obj = new greeting();
+//         obj.say("Arvind Sharma"); 
+//     }
+// }
+
+// Q4. Create a class Circle with a method calculateArea() that calculates and prints the area of a circle.
+
+// public class circle {
+
+//     public void circlearea() {
+//         double radius = 7;
+//         double pi = 3.14;
+//         double area = pi * radius * radius;
+//         System.out.println("area=" + area);
+//     }
+
+//     public static void main(String[] args) {
+//         circle obj = new circle();
+//         obj.circlearea();
+//     }
+// }
+
+
+// Q5. Create a class SimpleInterest with a method calculateInterest() that calculates simple interest.
+
+
+// public class si {
+
+//     public void cali() {
+//         float principal = 1000;
+//         float time = 2;
+//         float rate = 5;
+//         float si = (principal * time * rate) / 100;
+//         System.out.println("SI=" + si);
+//     }
+
+//     public static void main(String[] args) {
+//         si obj = new si();
+//         obj.cali();
+//     }
+// }
